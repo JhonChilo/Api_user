935132e01850e9bcc4d7d70120e8cab8fe8dc5d6
Sefedeam <sergio.delgado.a@utec.edu.pe>
2025-05-03 17:32:32 -0500

    Se agrego Dockerfile y docker-compose.yml

--- core/database.py
+++ core/database.py
@@ -1,8 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://Jhon:Erick2017@localhost:3306/api_user"
+# Obtén la URL de la base de datos desde las variables de entorno
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 # Crea el motor de conexión
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"charset": "utf8mb4"})
@@ -15,8 +17,8 @@ Base = declarative_base()
 
 # Crea la función para obtener la sesión de base de datos
 def get_db():
-    db = SessionLocal()  # Obtiene una sesión nueva de la base de datos
+    db = SessionLocal()
     try:
-        yield db  # Devuelve la sesión para usarla en las rutas
+        yield db
     finally:
-        db.close()  # Cierra la sesión al final
+        db.close()
