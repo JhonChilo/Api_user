da8b6f3fdff3ce9cff426f936511225aaf89de84
Sefedeam <sergio.delgado.a@utec.edu.pe>
2025-05-03 22:06:49 -0500

    Se arreglo un problema con las rutas del controlador de users. Incorporacion de postman_collecion.json

--- users/controller.py
+++ users/controller.py
@@ -8,26 +8,26 @@ from core.database import get_db
 router = APIRouter()
 
 # ---------------- LEER USUARIO ----------------
-@router.get("/users/{user_id}", response_model=schemas.User)
+@router.get("/{user_id}", response_model=schemas.User)
 def get_user(user_id: int, db: Session = Depends(get_db)):
     return service.get_user(db, user_id)
 
 # ---------------- LEER TODOS LOS USUARIOS ----------------
-@router.get("/users/", response_model=List[schemas.User])
+@router.get("", response_model=List[schemas.User])
 def get_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     return service.get_users(db, skip, limit)
 
-@router.put("/users/{user_id}", response_model=schemas.User)
+@router.put("/{user_id}", response_model=schemas.User)
 def update_user(user_id: int, user_update: schemas.UserUpdate, db: Session = Depends(get_db)):
     current_user_role = "admin"  # Este valor debería ser dinámico según el usuario autenticado
     return service.update_user(db, user_id, user_update, current_user_role)
 
-@router.delete("/users/{user_id}")
+@router.delete("/{user_id}")
 def delete_user(user_id: int, db: Session = Depends(get_db)):
     return service.delete_user(db, user_id)
 
 # ---------------- AÑADIR O ACTUALIZAR DIRECCIÓN ----------------
-@router.post("/users/{user_id}/address", response_model=Address)
+@router.post("/{user_id}/address", response_model=Address)
 def add_or_update_address(user_id: int, address_data: AddressCreate, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == user_id).first()
     if not user:
