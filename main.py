767403942ba97daf3fff7ac159c9033b1aaed86b
Sefedeam <sergio.delgado.a@utec.edu.pe>
2025-05-09 00:13:44 -0500

    Actualizando archivos de api usuarios

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from users.controller import router as users_router
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],      # Permite todos los orígenes
+    allow_credentials=True,
+    allow_methods=["*"],      # Permite todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],      # Permite todos los headers
+)
+
 # Registrar los routers para las rutas de autenticación y usuarios
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(users_router, prefix="/users", tags=["users"])
