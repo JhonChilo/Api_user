51500176a8d9c44d32095f3b3159cb6e4deb5df6
Sefedeam <sergio.delgado.a@utec.edu.pe>
2025-05-13 04:53:23 -0500

    Permitimos el consumo de la api habilitando el CORS

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from auth.controller import router as auth_router
 from users.controller import router as users_router
 
 app = FastAPI()
+
+# Habilitar CORS para cualquier origen
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite cualquier origen
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Registrar los routers para las rutas de autenticación y usuarios
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
